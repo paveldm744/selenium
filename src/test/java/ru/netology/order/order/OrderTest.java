59a2a9481f5bd07ca720570d55b45614c780c639
Павел Козодаев <paveldm744@gmail.com>
2022-10-14 14:46:38 +0300

    added test2

--- src/test/java/ru/netology/order/order/OrderTest.java
+++ src/test/java/ru/netology/order/order/OrderTest.java
@@ -17,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
     WebDriver driver;
-
     @BeforeAll
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -37,13 +36,24 @@ public class OrderTest {
     }
 
     @Test
-    void test() {
+    void shouldTestWithValidValue() {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
+    }
+
+    @Test
+    void shouldTestValidWithHyphen() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов-Петров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
     }
 }
